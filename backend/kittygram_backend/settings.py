c445bdaebe8564b4a2c9e1056dfd2fafaa19e0ff
Andrey Efremenkov <klontarf@yandex.ru>
2025-02-12 12:30:45 +0300

    settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'uq1!4$-89$')
 
 DEBUG = bool(os.getenv('DEBUG', False))
 
